72baf966ab0e702aafa444899f813f5fa219d7f5
Jorge L. Williams <jorge.williams@rackspace.com>
2013-02-15 23:17:22 -0600

    More complete message samples test.

--- src/test/scala/sample-messages-tests.scala
+++ src/test/scala/sample-messages-tests.scala
@@ -1,6 +1,7 @@
 package com.rackspace.usage
 
 import java.io.File
+import java.io.StringWriter
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -16,17 +17,50 @@ import com.rackspace.com.papi.components.checker.servlet.RequestAttributes._
 
 import BaseUsageSuite._
 
+import javax.xml.transform._
+import javax.xml.transform.stream.StreamSource
+import javax.xml.transform.stream.StreamResult
+
 //
 //  Test to make sure that all sample messages are valid against the
-//  product schemas.
+//  product schemas and that all samples can pass the validator in
+//  their respective feeds.
 //
 
 @RunWith(classOf[JUnitRunner])
 class SampleMessagesSuite extends BaseUsageSuite {
-  test("All sample messages should be valid according to the product schema") {
-    new File("message_samples").listFiles().filter(f => f.getName().endsWith(".xml")).foreach ( f => {
-      println("Checking message_samples/"+f.getName())
+
+  val sampleDir   = new File("message_samples")
+  val sampleFiles = sampleDir.listFiles.filter(_.getName().endsWith(".xml"))
+
+  test("All sample files should be valid according to the product schema") {
+    sampleFiles.foreach ( f => {
+      printf("Checking %s/%s\n",sampleDir.getName,f.getName)
       usageMsg.assert(XML.loadFile(f))
     })
+  }
+
+  test("All sample files annotated with <?feed ..> should pass validation on that feed") {
+    sampleFiles.map(toFeedFile).filter(_ != None).map(_.get).foreach (f => {
+      printf("Checking %s/%s against feed %s\n",sampleDir.getName, f._2.getName, f._1)
+      atomValidator.validate(request("POST", f._1, "application/atom+xml", XML.loadFile(f._2)), response, chain)
+    })
+  }
+
+  val templates = TransformerFactory.newInstance().newTemplates(new StreamSource(new File("src/test/resources/get-feed.xsl").toURI.toString))
+
+  //
+  //  Converts a sample file to an optional (feed/path, file)
+  //
+  def toFeedFile (f : File) : Option[(String, File)] = {
+    val trans = templates.newTransformer()
+    val writer = new StringWriter()
+
+    trans.transform(new StreamSource(f), new StreamResult(writer))
+    if (writer.toString().isEmpty()) {
+      None
+    } else {
+      Some((writer.toString, f))
+    }
   }
 }
