75fef5283ddebef28628c0d35b80e21abe467382
Shinta Smith <shinta.smith@rackspace.com>
2013-03-18 10:08:38 -0500

    [B-44127][TK-77618] added unit test for LBaaS SSL=ON rule

--- src/test/scala/lbaas-stopgap-tests.scala
+++ src/test/scala/lbaas-stopgap-tests.scala
@@ -59,6 +59,24 @@ class LbaasStopGapSuite extends BaseUsageSuite {
                                   </atom:content>
                                 </atom:entry>
 
+  var lbaasSSLOnWithNonSSLData = <atom:entry xmlns="http://docs.rackspace.com/core/event" xmlns:atom="http://www.w3.org/2005/Atom"
+                                             xmlns:lbaas="http://docs.rackspace.com/usage/lbaas">
+                                             <atom:title type="text">LBAAS</atom:title>
+                                             <atom:content type="application/xml">
+                                                 <event resourceName="LoadBalancer" endTime="2012-06-15T10:19:52Z"
+                                                        startTime="2012-06-14T10:19:52Z" region="DFW" dataCenter="DFW1" type="USAGE"
+                                                        id="b79cc3de-b399-3883-b555-61829bb7f966"
+                                                        resourceId="b79cc3de-b399-3883-b555-61829bbccd38" tenantId="1" version="1">
+                                                     <lbaas:product sslMode="ON" vipType="PUBLIC" numVips="44" numPolls="10" bandWidthOutSsl="345345346"
+                                                                    bandWidthInSsl="364646770" bandWidthOut="1000" bandWidthIn="1000"
+                                                                    avgConcurrentConnectionsSsl="4566.0"
+                                                                    avgConcurrentConnections="1000" serviceCode="CloudLoadBalancers"
+                                                                    resourceType="LOADBALANCER" status="ACTIVE"
+                                                                    version="1"/>
+                                                     </event>
+                                                 </atom:content>
+                                             </atom:entry>
+
   val lbaasSSLMixed = <atom:entry xmlns="http://docs.rackspace.com/core/event" xmlns:atom="http://www.w3.org/2005/Atom"
                                 xmlns:lbaas="http://docs.rackspace.com/usage/lbaas">
                                 <atom:title type="text">LBAAS</atom:title>
@@ -91,6 +109,14 @@ class LbaasStopGapSuite extends BaseUsageSuite {
     assert(getProcessedXML(req), "/atom:entry/atom:content/event:event/lbaas:product/@bandWidthInSsl = '364646770'")
     assert(getProcessedXML(req), "/atom:entry/atom:content/event:event/lbaas:product/@bandWidthOutSsl = '345345346'")
     assert(getProcessedXML(req), "/atom:entry/atom:content/event:event/lbaas:product/@avgConcurrentConnectionsSsl = '4566.0'")
+  }
+
+  test ("A usage event with SSL ON submitted with Non-SSL non-zero data to LBAAS feed (lbaas/events) should set bandWidthIn, bandWidthOut, and avgConcurrentConnections to zero") {
+      val req = request("POST", "/lbaas/events", "application/atom+xml", lbaasSSLOnWithNonSSLData)
+      atomValidator.validate(req, response, chain)
+      assert(getProcessedXML(req), "/atom:entry/atom:content/event:event/lbaas:product/@bandWidthIn = '0'")
+      assert(getProcessedXML(req), "/atom:entry/atom:content/event:event/lbaas:product/@bandWidthOut = '0'")
+      assert(getProcessedXML(req), "/atom:entry/atom:content/event:event/lbaas:product/@avgConcurrentConnections = '0'")
   }
 
   test ("A usage event with SSL MIXED submitted to LBAAS feed (lbaas/events) should NOT set bandWidthInSsl, bandWidthOutSsl, and avgConcurrentConnectionsSsl to zero") {
