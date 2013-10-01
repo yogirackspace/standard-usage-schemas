871301efc4ba30fe8ede9bb551d4b1c6ff02fe57
Shinta Smith <shinta.smith@rackspace.com>
2013-10-01 11:13:56 -0500

    D-15294: rename atom_hopper_identity.wadl

--- src/test/scala/test-base.scala
+++ src/test/scala/test-base.scala
@@ -53,7 +53,7 @@ object BaseUsageSuite {
   //  The atom hopper validator
   //
   val atomValidator = Validator(new StreamSource(new File("atom_hopper.wadl")), usageConfig)
-  val atomValidatorIdentity = Validator(new StreamSource(new File("atom_hopper_identity.wadl")), usageConfig)
+  val atomValidatorIdentity = Validator(new StreamSource(new File("atom_hopper_identity_admin.wadl")), usageConfig)
 
   //
   //  Convinece function to get to the XML of a request
