c000847b3e40bfbe477cb8d50638ffb2b435ab76
Shinta Smith <shinta.smith@RACKSPACE.COM>
2014-10-16 18:12:51 -0500

    update to make it work

--- src/test/scala/ValidateWithRoles.scala
+++ src/test/scala/ValidateWithRoles.scala
@@ -18,12 +18,12 @@ class ValidateWithRoles extends BaseUsageSuite {
       Map("ACCEPT"->List("application/xml"), "X-ROLES" ->List("cloudfeeds:service-admin")) ), response, chain)
   }
 
-  test("should pass when getting bigdata/events/12345 feed JSON Content with Accept: application/xml and X-ROLES:cloudfeeds:observer ") {
+  test("should pass when getting bigdata/events/12345 feed with Accept: application/xml and X-ROLES:cloudfeeds:observer ") {
     atomValidator.validate(request("GET", "bigdata/events/12345", "", "", false,
       Map("ACCEPT"->List("application/xml"), "X-ROLES" ->List("cloudfeeds:observer")) ), response, chain)
   }
 
-  test("should pass when GET with /bigdata entry JSON Content as observer with Accept application/xml") {
+  test("should pass when GET with /bigdata entry as observer with Accept application/xml") {
     atomValidator.validate(request("GET", "bigdata/events/12345/entries/urn:uuid:8d89673c-c989-11e1-895a-0b3d632a8a89", "", "",
       false, Map("ACCEPT"->List("application/xml"), "X-ROLES" ->List("cloudfeeds:observer")) ), response, chain)
   }
